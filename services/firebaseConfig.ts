79eee710ac44e36a34e294ac7bcb4e7b09e2fe14
shivamsid-47 <sidharthshivam7@gmail.com>
2026-01-08 21:02:57 +0530

    Enhance Firebase config with auth persistence
    Added persistence for authentication and handled analytics initialization conditionally.

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import {getAuth,
+  setPersistence,
+  browserLocalPersistence} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -16,9 +18,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const analytics =
+  typeof window !== "undefined" ? getAnalytics(app) : null;
+
 
 // Export services
 export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
+  .catch((err) => console.error("Auth persistence error:", err));
 export const db = getFirestore(app);
 export { analytics };
+
